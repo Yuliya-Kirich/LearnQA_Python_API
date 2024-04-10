f10f402145378dcbfb6a2bcc9ea815a84b92bb42
yk <yk@192.168.1.4>
2024-04-10 12:24:12 +0300

    Ex-16

--- tests/test_user_register.py
+++ tests/test_user_register.py
@@ -2,10 +2,28 @@ import pytest
 import requests
 from lib.base_case import BaseCase
 from lib.assertions import Assertions
-
+from datetime import datetime
 
 class TestUserRegister(BaseCase):
     base_url = "https://playground.learnqa.ru/api/user/"
+
+    def setup_method(self):
+        base_part = "learnqa"
+        domain = "example.com"
+        random_part = datetime.now().strftime("%m%d%Y%H%M%S")
+        self.email = f"{base_part}{random_part}@{domain}"
+
+    def test_create_user_successfully(self):
+        data = {
+            "username": "user1",
+            "firstName": "John",
+            "lastName": "Doe",
+            "email": self.email,
+            "password": "123"
+        }
+        response = requests.post("https://playground.learnqa.ru/api/user/", data=data)
+        Assertions.assert_code_status(response, 200)
+        Assertions.assert_json_has_key(response, "id")
 
     def test_create_user_with_incorrect_email(self):
         email = 'incorrectemail.com'
@@ -18,25 +36,15 @@ class TestUserRegister(BaseCase):
         }
         response = requests.post(self.base_url, data=data)
         Assertions.assert_code_status(response, 400)
-        assert response.content.decode(
-            "utf-8") == f"Invalid email format", f"Unexpected response content {response.content}"
+        assert response.content.decode("utf-8") == "Invalid email format", f"Unexpected response content {response.content}"
 
-    missing_fields = [
+    @pytest.mark.parametrize("missing_field", [
         "username",
         "firstName",
         "lastName",
         "email",
         "password"
-    ]
-    data = {
-        "username": "user2",
-        "firstName": "Jane",
-        "lastName": "Doe",
-        "email": "jane@example.com",
-        "password": "123"
-    }
-
-    @pytest.mark.parametrize("missing_field", missing_fields)
+    ])
     def test_create_user_missing_field(self, missing_field):
         data = {
             "username": "user2",
